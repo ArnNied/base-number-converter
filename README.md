8f4642362caeade2adcf8e8234707b3aaa5e15c0
Andrien Wiandyano <arnnied03@gmail.com>
2021-10-05 21:35:53 +0700

    refactor: overhauled the whole structur for an (maybe) easier reading

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 Convert from base 2-36 to base 2-36.
 
 ## USAGE
-Input: {original_base} {number_to_be_converted} {intended_base}
-
-{original_base}: The base format of the number that will be converted.
+Input: {number_to_be_converted} {original_base} {intended_base}
 
 {number_to_be_converted}: Self-explanatory.
+
+{original_base}: The base format of the number that will be converted.
 
 {intended_base}: The base format the number will be converted to.
 
@@ -16,12 +16,12 @@ Input: {original_base} {number_to_be_converted} {intended_base}
 
 ## EXAMPLES
 ```
-> 10 2748 16
+> 2748 10 16
 ABC
 
-> 36 737RA 10
+> 737RA 36 10
 11907334
 
-> 32 hello 2
+> hello 32 2
 1000101110101011010111000
 ```
--- converter.py
+++ converter.py
@@ -1,45 +1,106 @@
 from string import digits, ascii_uppercase
-from math import floor
-
-chars_list = list(digits + ascii_uppercase)
 
 
-print(f"Order (ltr): {''.join(chars_list)}")
-print("Format: [original_base] [number_to_be_converted] [intended_base]\nEx: 10 2748 16 -> ABC")
+class Converter:
+    """
+    Converts a number from one base to another
+    """
+
+    chars_list = digits + ascii_uppercase
+
+    original_base = None
+    submitted_number = None
+    intended_base = None
+
+    original_base_chars = None
+    intended_base_chars = None
+
+    calculated_decimal = 0
+    output = list()
+
+    def __init__(self, data) -> None:
+        submitted_number, original_base, intended_base = data.split()
+        self.submitted_number = tuple(submitted_number.upper())
+        self.original_base = int(original_base) 
+        self.intended_base = int(intended_base)
+
+    def assign_chars(self) -> None:
+        """
+        Assign usable chars for original and intended base.
+        """
+
+        self.original_base_chars = self.chars_list[:self.original_base]
+        self.intended_base_chars = self.chars_list[:self.intended_base]
+
+    def validation(self) -> None:
+        """
+        Check if self.original_base or self.intended_base is out of range.
+        """
+
+        if self.original_base > len(self.chars_list) or self.original_base < 2:
+            raise Exception(f"[original_base] exceed the allowed base (2 - {len(self.chars_list)})")
+
+        if self.intended_base > len(self.chars_list) or self.intended_base < 2:
+            raise Exception(f"[intended_base] exceed the allowed base (2 - {len(self.chars_list)})")
+
+    def convert_to_decimal(self) -> None:
+        """
+        Convert self.submitted_number to decimal.
+        """
+
+        for i in range(len(self.submitted_number)):
+            current_num = self.submitted_number[0 - (i+1)]
+
+            print(current_num)
+
+            if current_num not in self.original_base_chars:
+                raise ValueError("[number_to_be_converted] does not follow the [original_base] format")
+            
+            original_value_in_decimal = self.original_base_chars.index(current_num)
+            intended_value = original_value_in_decimal * (self.original_base**i)
+            
+            self.calculated_decimal += intended_value
+
+    def calculate_output(self) -> None:
+        """
+        Calculate desired output.
+        """
+
+        calculated_decimal = self.calculated_decimal
+        while calculated_decimal != 0:
+            calculated_decimal, remainder = divmod(calculated_decimal, self.intended_base)
+            self.output.append(self.intended_base_chars[remainder])
+    
+    def reverse_and_join_output(self) -> str:
+        """
+        Reverse then join the output because the previous calculation is done in reverse.
+        """
+
+        self.output.reverse()
+        return "".join(self.output)
+
+    def execute(self) -> str:
+        """
+        Entry point.
+        """
+
+        self.validation()
+        self.assign_chars()
+        self.convert_to_decimal()
+        self.calculate_output()
+
+        return self.reverse_and_join_output()
+
+print(
+"""
+======================================
+        BASE NUMBER CONVERTER
+======================================
+""")
+print(f"Ordering (ltr): {Converter.chars_list}")
+print("Input: [number_to_be_converted] [original_base] [intended_base]")
+print("Ex: 2748 10 16 -> ABC ")
 
 while True:
-    original_base, submitted_number, intended_base = input("> ").split()
-
-    # SETUP
-    original_base = int(original_base)
-    intended_base = int(intended_base)
-    submitted_number_list = list(submitted_number.upper())
-
-    original_base_chars = chars_list[:original_base]
-    intended_base_chars = chars_list[:intended_base]
-
-
-    # EXCEPTIONS
-    if original_base > len(chars_list) or intended_base > len(chars_list):
-        raise Exception(f"[original_base] and/or [intended_base] exceed the allowed base ({len(chars_list)})")
-
-    for i in submitted_number_list:
-        if i not in original_base_chars:
-            raise Exception("[number_to_be_converted] does not follow the [original_base] format")
-
-
-    # CONVERT THE ORIGINAL NUMBER TO DECIMAL FIRST FOR LIST EASY ACCESS
-    calc_decimal = 0
-    for i in range(len(submitted_number_list)):
-        calc_decimal += original_base_chars.index(submitted_number_list[0 - (i+1)]) * (original_base**i)
-
-
-    # CALCULATING THE OUTPUT
-    output = list()
-    while calc_decimal != 0:
-        calc_decimal, remainder = divmod(calc_decimal, intended_base)
-        output.append(intended_base_chars[remainder])
-
-    # REVERSE THEN JOIN THE LIST TO CREATE A STRING BECAUSE THE PREVIOUS CALCULATION IS DONE IN REVERSE
-    output.reverse()
-    print(f"{''.join(output)}\n")
+    user_input = input("\n> ")
+    print(Converter(user_input).execute())
